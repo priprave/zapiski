caeebe18935c9290e6e57cf70f105b6ac41742ea
Patrik Žnidaršič <posta.patrik.2@gmail.com>
2024-11-27 18:15:03 +0100

    zapiski za zanke

---
+++ poglavja/zanke/sintaksa.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    //   začetek;      pogoj;      korak
+    for (int stevec=0; stevec < 3; stevec++) {
+        // med zavitimi oklepaji {} je koda,
+        // ki se izvede vsako iteracijo zanke
+        printf("nekaj\n");
+    }
+    return 0;
+}
